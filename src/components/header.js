9ef6099c371aade08ec7dbd157c2c1f9759e5e1a
Dekoher <diegoherr92@gmail.com>
2020-06-25 23:50:26 -0500

    my first commit with header and footer

---
+++ src/components/header.js
@@ -0,0 +1,55 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import { MenuItem, StyledHeader } from "../styles/components";
+
+const Header = ({ siteTitle }) => (
+  <StyledHeader>
+    <Link to='/'>
+      <MenuItem margin>
+        <Link to='/'>E-commerce</Link>
+      </MenuItem>
+      {/* <img src='https://i.postimg.cc/6q3pg48v/Logo.png' alt='logoreptiliadaniel'></img> */}
+    </Link>
+    <nav>
+      <ul>
+        <MenuItem margin>
+          <Link to='/'>Productos</Link>
+        </MenuItem>
+        <MenuItem margin>
+          <a href='https://www.gatsbyjs.org'>Gatsby</a>
+        </MenuItem>
+        <MenuItem>
+          <Link to='/cart'>
+            <span>
+              <img src='https://i.postimg.cc/L6wpMxLt/cart.png' alt='cartlogo'></img>
+            </span>
+          </Link>
+        </MenuItem>
+      </ul>
+    </nav>
+    <div>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </StyledHeader>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/components/layout.js
@@ -0,0 +1,34 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import { Content, Footer } from '../styles/components';
+
+import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <Content>
+      <main>{children}</main>
+      <Footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </Footer>
+    </Content>
+  </>
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
